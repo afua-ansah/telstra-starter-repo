c92b8520ce327d829bb143ec0d7d69b67f466abd
Afua M.  Ansah <ama376@cornell.edu>
2024-09-27 07:46:53 -0700

    Add a persitence layer to save SIM card records containing the ICCID of the SIM card, the associated customer's email address, and a boolean value indicating whether the SIM card was activated successfully

--- src/main/java/au/com/telstra/simcardactivator/SimCard.java
+++ src/main/java/au/com/telstra/simcardactivator/SimCard.java
@@ -1,32 +1,56 @@
 package au.com.telstra.simcardactivator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class SimCard {
-    private String iccd;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    
+    private String iccid;
     private String customerEmail;
+    private boolean activationSuccess;
 
     public SimCard() {
 
     }
-    
-    public SimCard(String iccd, String customerEmail) {
-        this.iccd = iccd;
+
+    public SimCard(String iccid, String customerEmail, boolean active) {
+        this.iccid = iccid;
         this.customerEmail = customerEmail;
+        this.activationSuccess = active;
     }
 
-    public String getIccd() {
-        return iccd;
+    public String getIccid() {
+        return iccid;
     }
 
     public String getCustomerEmail() {
         return customerEmail;
     }
 
-    public void setIccd(String iccd) {
-        this.iccd = iccd;
+    public Long getId() {
+        return id;
+    }
+
+    public boolean getActivationSuccess() {
+        return activationSuccess;
+    }
+
+    public void setIccd(String iccid) {
+        this.iccid = iccid;
     }
 
     public void setCustomerEmail(String customerEmail) {
         this.customerEmail = customerEmail;
+    }
+
+    public void setActivationSuccess(boolean activationSuccess) {
+        this.activationSuccess = activationSuccess;
     }
 
 }
--- src/main/java/au/com/telstra/simcardactivator/SimCardActivatorController.java
+++ src/main/java/au/com/telstra/simcardactivator/SimCardActivatorController.java
@@ -1,9 +1,11 @@
 package au.com.telstra.simcardactivator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +14,22 @@ public class SimCardActivatorController {
     @Autowired
     private SimCardActivationHandler activationHandler;
 
+    @Autowired
+    private SimCardRepository repository;
+
     @PostMapping(path = "/", consumes = "application/json")
     public void routeSimActivation(@RequestBody SimCard simCard){
         ActivationResult activationResult = activationHandler.activate(simCard);
-        System.out.println(activationResult.getSuccess());
+        String iccid = simCard.getIccid();
+        String email = simCard.getCustomerEmail();
+        boolean active = activationResult.getSuccess();
+        repository.save(new SimCard(iccid, email, active));
+    }
+
+    @GetMapping(path = "/", produces = "application/json")
+    public SimCard getSimCard(@RequestParam long simCardId) {
+        SimCard simCard = repository.findById(simCardId);
+        return simCard;
     }
 
 }
